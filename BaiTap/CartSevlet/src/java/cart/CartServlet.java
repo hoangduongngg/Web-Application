d499d087e0f8ac9235ea5b9bcc4a37f08bfa281b
Hoang Duong Nguyen <hoangduongnguyen.tech@gmail.com>
2022-03-25 00:22:48 +0700

    Edit code

--- BaiTap/CartSevlet/src/java/cart/CartServlet.java
+++ BaiTap/CartSevlet/src/java/cart/CartServlet.java
@@ -3,38 +3,34 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import business.*;
-//import data.*;
+import data.*;
 public class CartServlet extends HttpServlet
 {
     protected void doGet(HttpServletRequest request, 
                         HttpServletResponse response)
                         throws ServletException, 
-                        IOException
+                        IOException 
     {
         String productCode = request.getParameter("productCode");
         String quantityString = request.getParameter("quantity");
         HttpSession session = request.getSession();
         Cart cart = (Cart) session.getAttribute("cart");
         if (cart == null)
-        cart = new Cart();
+            cart = new Cart();
         int quantity = 1;
-        try
-        {
+        try {
             quantity = Integer.parseInt(quantityString);
             if (quantity < 0)
             quantity = 1;
         }
-        catch(NumberFormatException nfe)
-        {
+        catch(NumberFormatException nfe){
             quantity = 1;
         }
         ServletContext sc = getServletContext();
-        String path = 
-            sc.getRealPath("WEB-INF/products.txt");
-//        Product product = 
-//            ProductIO.getProduct(productCode, path);
+        String path = sc.getRealPath("WEB-INF/products.txt");
+        Product product = ProductIO.getProduct(productCode, path);
         LineItem lineItem = new LineItem();
-//        lineItem.setProduct(product);
+        lineItem.setProduct(product);
         lineItem.setQuantity(quantity);
         if (quantity > 0)
             cart.addItem(lineItem);
--- BaiTap/CartSevlet/src/java/data/ProductIO.java
+++ BaiTap/CartSevlet/src/java/data/ProductIO.java
@@ -1,5 +1,4 @@
 package data;
-
 /**
  *
  * @author hoangduongngg
