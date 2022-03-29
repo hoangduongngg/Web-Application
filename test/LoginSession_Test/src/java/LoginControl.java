d62b8140d2c8594d3de63348f0e6d2127bbf0ffc
Hoang Duong Nguyen <hoangduongnguyen.tech@gmail.com>
2022-03-30 00:17:33 +0700

    Create LoginControl use session
    set data from file jsp to session, and forward to showAccout.jsp

---
+++ test/LoginSession_Test/src/java/LoginControl.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+/**
+ *
+ * @author hoangduongngg
+ */
+@WebServlet(urlPatterns = {"/login"})
+public class LoginControl extends HttpServlet{
+    protected void processRequest (HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException{
+        response.setContentType("text/html;charset=UTF-8");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+//        request.setAttribute("user", username);
+//        request.setAttribute("pass", password);
+
+        HttpSession session = request.getSession();
+        session.setAttribute("user", username);
+        session.setAttribute("pass", password);
+        request.getRequestDispatcher("showAccount.jsp").forward(request, response); 
+    }
+    
+    protected void doPost (HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
