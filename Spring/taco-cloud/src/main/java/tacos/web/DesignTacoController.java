80d002780d0c8220e0f658f933443ee4d6f68b6c
Hoang Duong Nguyen <hoangduongnguyen.tech@gmail.com>
2022-04-15 15:17:51 +0700

    Clean code and add Validation
    In processDesign, add var Errors errors

--- Spring/taco-cloud/src/main/java/tacos/web/DesignTacoController.java
+++ Spring/taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -22,12 +22,17 @@ import tacos.Ingredient.Type;
 public class DesignTacoController {
 	@ModelAttribute
 	public void addIngredientsToModel(Model model) {
-		  List<Ingredient> ingredients = Arrays.asList(new 		      Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-				new Ingredient("COTO", "Corn Tortilla", Type.WRAP), new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+		  List<Ingredient> ingredients = Arrays.asList(
+				new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+				new Ingredient("COTO", "Corn Tortilla", Type.WRAP), 
+				new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
 				new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-				new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES), new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-				new Ingredient("CHED", "Cheddar", Type.CHEESE), new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-				new Ingredient("SLSA", "Salsa", Type.SAUCE), new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+				new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES), 
+				new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+				new Ingredient("CHED", "Cheddar", Type.CHEESE), 
+				new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+				new Ingredient("SLSA", "Salsa", Type.SAUCE), 
+				new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
 	
 			Type[] types = Ingredient.Type.values();
 			for (Type type : types) {
@@ -50,11 +55,11 @@ public class DesignTacoController {
 				
 		}
 		@PostMapping
-		public String processDesign(Taco taco) {
-			// Thuc hien Validate, bo cmt 2 dong duoi thi neu co loi se tra ve trang design
-//			if (errors.hasErrors()) {
-//			    return "design";
-//			}
+		public String processDesign(Taco taco, Errors errors) {
+			// Validate,
+			if (errors.hasErrors()) {
+			    return "design";
+			}
 
 		  // Save the taco design...
 		  // We'll do this later
