a091fa05e797c9532ea2438858cc8b8378ef82ee
Hoang Duong Nguyen <hoangduongnguyen.tech@gmail.com>
2022-04-26 00:07:37 +0700

    Rename

---
+++ BaiTap/BaiThucHanh2/src/java/data/DBConnection.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+/**
+ *
+ * @author hoangduongngg
+ */
+public class DBConnection {
+    public static Connection getConnection(String strServer, String strDatabase, String username, String password){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+//            Class.forName("com.mysql.jdbc.Driver"); //or
+            String strCon = "jdbc:mysql://"+strServer+":3306/"+strDatabase;
+            return DriverManager.getConnection(strCon, username, password);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+}
